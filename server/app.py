711dcd0e15a76ac9ed7a688db040a706121fc543
mrrsmburu <mrrsmburu@gmail.com>
2024-07-10 12:14:45 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -15,6 +15,29 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
+ma = Marshmallow(app)
+class NewsletterSchema(ma.SQLAlchemySchema):
+
+    class Meta:
+        model = Newsletter
+        load_instance = True
+
+    title = ma.auto_field()
+    published_at = ma.auto_field()
+
+
+    url = ma.Hyperlinks(
+        {
+            "self": ma.URLFor(
+                "newsletterbyid",
+                values=dict(id="<id>")),
+            "collection": ma.URLFor("newsletters"),
+        }
+    )
+
+newsletter_schema = NewsletterSchema()
+newsletters_schema = NewsletterSchema(many=True)
+
 api = Api(app)
 
 class Index(Resource):
@@ -38,10 +61,10 @@ class Newsletters(Resource):
 
     def get(self):
         
-        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+        newsletters = Newsletter.query.all()
 
         response = make_response(
-            response_dict_list,
+            newsletters_schema.dump(newsletters),
             200,
         )
 
@@ -57,12 +80,11 @@ class Newsletters(Resource):
         db.session.add(new_record)
         db.session.commit()
 
-        response_dict = new_record.to_dict()
-
         response = make_response(
-            response_dict,
+            newsletter_schema.dump(new_newsletter),
             201,
         )
+        
 
         return response
 
@@ -72,32 +94,33 @@ class NewsletterByID(Resource):
 
     def get(self, id):
 
-        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+        newsletter = Newsletter.query.filter_by(id=id).first()
 
         response = make_response(
-            response_dict,
+            newsletter_schema.dump(newsletter),
             200,
         )
 
         return response
 
+        return response
+
     def patch(self, id):
 
-        record = Newsletter.query.filter_by(id=id).first()
+        newsletter = Newsletter.query.filter_by(id=id).first()
         for attr in request.form:
-            setattr(record, attr, request.form[attr])
+            setattr(newsletter, attr, request.form[attr])
 
-        db.session.add(record)
+        db.session.add(newsletter)
         db.session.commit()
 
-        response_dict = record.to_dict()
-
         response = make_response(
-            response_dict,
+            newsletter_schema.dump(newsletter),
             200
         )
 
         return response
+    
 
     def delete(self, id):
 
